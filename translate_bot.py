08b35aa2fa4ab86b701a6eb74a3a6334926eae6d
charlenemayy <charlenemayy@knights.ucf.edu>
2018-08-29 18:23:14 -0400

    added arabic, spanish, japanese and tagalog

--- translate_bot.py
+++ translate_bot.py
@@ -1,52 +1,54 @@
+'''
+ Charlene May
+ translates typed messages in Telegram text box
+ (languages recommended by friends) and sends
+ the resulted translated text as a message
+'''
 from telegram.ext import Updater
 from telegram import InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import InlineQueryHandler
 from googletrans import Translator
 import logging
+import uuid
 
 logging.basicConfig(format='%(asctime)s - %(name)s - '
                     + '%(levelname)s - %(message)s', level=logging.INFO)
 
-# updates stored on server until bot recieves them
 updater = Updater(token='649269167:AAHkrWbTcMYRlbg9mqCwB6x--uavzL_S2TU')
 dispatcher = updater.dispatcher
 
-# initalize translate api
+# intialize google translate api
 translator = Translator()
 
+# don't want to import all available languages yet
+languages = {'en': 'English', 'ru': 'Russian', 'es': 'Spanish',
+             'ar': 'Arabic', 'fil': 'Tagalog', 'ja': 'Japanese'}
 
-# inline handler
-# eventually make dynamic instead
-# of having to write new translate line per language
+
+# to-do: slow loading time for bot; improve efficiency
+# inline handler; loads available languages as buttons; change
+# to horizontal format
 def inline_translate(bot, update):
-    query = update.inline_query.query  # text of the query
+    query = update.inline_query.query
     if not query:
         return
     results = list()
-    # russian
-    ru_text = translator.translate(query, dest='ru').text
-    results.append(
-        InlineQueryResultArticle(
-            id=ru_text,
-            title='Russian',
-            input_message_content=InputTextMessageContent(ru_text)
+    for k, v in languages.items():
+        trans_text = translator.translate(query, dest=k).text
+        results.append(
+            InlineQueryResultArticle(
+                id=uuid.uuid4(),
+                title=v,
+                input_message_content=InputTextMessageContent(trans_text)
+            )
         )
-    )
-    # english
-    en_text = translator.translate(query, dest='en').text
-    results.append(
-        InlineQueryResultArticle(
-            id=en_text,
-            title='English',
-            input_message_content=InputTextMessageContent(en_text)
-        )
-    )
+
     # shows the results above text entry
     bot.answer_inline_query(update.inline_query.id, results)
 
 
+# add handler to telegram bot
 inline_translate_handler = InlineQueryHandler(inline_translate)
 dispatcher.add_handler(inline_translate_handler)
 
-# poll updates from Telegram
 updater.start_polling()
